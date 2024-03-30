6387046dcad02a25892561a328b294828d63ff59
luckzhangfengbo <1819067326@qq.com>
2024-03-30 13:41:44 +0800

    使用element ui实现了表格

--- js/index.js
+++ js/index.js
@@ -4,11 +4,22 @@
  * @Author: luckzhangfengbo
  * @Date: 2024-03-30 11:14:36
  * @LastEditors: zhangfengbo
- * @LastEditTime: 2024-03-30 11:15:05
+ * @LastEditTime: 2024-03-30 13:25:30
  */
 const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        students:[
+            {
+               sno:95001, name:'zhangfengbo', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+            },
+            {
+                sno:95002, name:'zhangfengbo1', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+            },
+            {
+                sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+            },
+        ]
     }
 })
