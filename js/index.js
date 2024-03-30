477453371d88dc13a0bb306a2aba90d8c8a22d42
luckzhangfengbo <1819067326@qq.com>
2024-03-30 15:25:21 +0800

    Vue中使用Axios请求后端端口

--- js/index.js
+++ js/index.js
@@ -4,46 +4,35 @@
  * @Author: luckzhangfengbo
  * @Date: 2024-03-30 11:14:36
  * @LastEditors: zhangfengbo
- * @LastEditTime: 2024-03-30 13:55:01
+ * @LastEditTime: 2024-03-30 15:22:07
  */
 const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
-        students:[
-            {
-               sno:95001, name:'zhangfengbo', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-            },
-            {
-                sno:95002, name:'zhangfengbo1', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-            },
-            {
-                sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-            },
-            {
-                sno:95001, name:'zhangfengbo', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-             },
-             {
-                 sno:95002, name:'zhangfengbo1', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-             },
-             {
-                 sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-             },
-             {
-                sno:95001, name:'zhangfengbo', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-             },
-             {
-                 sno:95002, name:'zhangfengbo1', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-             },
-             {
-                 sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-             },
-             {
-                sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
-            },
-        ],
+        msg: 'Hello Vue!',
+        baseURL:"http://localhost:8000/",
+        students:[],
         total:100, //数据总行数
         currentpage:1,//当前页码
         pagesize:10,//每页显示行数
-    }
+    },
+    mounted() {
+        //自动加载数据
+        this.getStudents();
+    },
+    methods: {
+        //获取所有学生的信息
+        getStudents:function(){
+            //使用Axios实现Ajax请求
+            axios.get(this.baseURL + "students/")
+            .then(function(res){
+                //成功
+                console.log(res);
+            })
+            .catch(function(err){
+                //失败
+                console.log(error);
+            });
+        },
+    },
 })
