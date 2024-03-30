92e2ee3de907a20787d710471f0989f8a89aa0af
luckzhangfengbo <1819067326@qq.com>
2024-03-30 13:58:19 +0800

    前端实现了下方批量删除与分页展示

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
  * @Author: luckzhangfengbo
  * @Date: 2024-03-30 11:14:36
  * @LastEditors: zhangfengbo
- * @LastEditTime: 2024-03-30 13:25:30
+ * @LastEditTime: 2024-03-30 13:55:01
  */
 const app = new Vue({
     el: '#app',
@@ -20,6 +20,30 @@ const app = new Vue({
             {
                 sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
             },
-        ]
+            {
+                sno:95001, name:'zhangfengbo', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+             },
+             {
+                 sno:95002, name:'zhangfengbo1', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+             },
+             {
+                 sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+             },
+             {
+                sno:95001, name:'zhangfengbo', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+             },
+             {
+                 sno:95002, name:'zhangfengbo1', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+             },
+             {
+                 sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+             },
+             {
+                sno:95003, name:'zhangfengbo2', gender:'男',birthday:'1995-01-01',mobile:'12345678901',email:'1819067324@qq.com',address:'北京市海淀区'
+            },
+        ],
+        total:100, //数据总行数
+        currentpage:1,//当前页码
+        pagesize:10,//每页显示行数
     }
 })
