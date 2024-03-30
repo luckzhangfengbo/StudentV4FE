d85a1022b3b57dc54f68788877b601b0a297fdf4
luckzhangfengbo <1819067326@qq.com>
2024-03-30 11:16:30 +0800

    vue初始化完成

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: luckzhangfengbo
+ * @Date: 2024-03-30 11:14:36
+ * @LastEditors: zhangfengbo
+ * @LastEditTime: 2024-03-30 11:15:05
+ */
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
