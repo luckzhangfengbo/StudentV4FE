0139456e7dd12405ddee6503830750895e09b95a
luckzhangfengbo <1819067326@qq.com>
2024-03-30 16:10:10 +0800

    解决Ajax跨域请求

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@
  * @Author: luckzhangfengbo
  * @Date: 2024-03-30 11:14:36
  * @LastEditors: zhangfengbo
- * @LastEditTime: 2024-03-30 15:22:07
+ * @LastEditTime: 2024-03-30 16:02:48
  */
 const app = new Vue({
     el: '#app',
     data: {
         msg: 'Hello Vue!',
-        baseURL:"http://localhost:8000/",
+        baseURL:"http://127.0.0.1:8000/",
         students:[],
         total:100, //数据总行数
         currentpage:1,//当前页码
@@ -24,14 +24,24 @@ const app = new Vue({
         //获取所有学生的信息
         getStudents:function(){
             //使用Axios实现Ajax请求
+            // axios.get(this.baseURL + "students/")
+            // .then(function(response){
+            //     //成功
+            //     console.log(response);
+            // })
+            // .catch(function(err){
+            //     //失败
+            //     console.log(error);
+            // });
+            //使用Axios实现Ajax请求
             axios.get(this.baseURL + "students/")
-            .then(function(res){
+            .then(response => {
                 //成功
-                console.log(res);
+                console.log(response);
             })
-            .catch(function(err){
+            .catch(err => {
                 //失败
-                console.log(error);
+                console.log(err);
             });
         },
     },
