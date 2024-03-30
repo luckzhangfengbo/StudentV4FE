5204a7f911eb140278008004078d9cc23bc48192
luckzhangfengbo <1819067326@qq.com>
2024-03-30 21:34:30 +0800

    完成学号是否存在的校验

--- js/index.js
+++ js/index.js
@@ -1,220 +1,270 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: luckzhangfengbo
  * @Date: 2024-03-30 11:14:36
  * @LastEditors: zhangfengbo
- * @LastEditTime: 2024-03-30 21:07:14
+ * @LastEditTime: 2024-03-30 21:30:11
  */
 const app = new Vue({
-    el: '#app',
-    data: {
-        msg: 'Hello Vue!',
-        baseURL:"http://127.0.0.1:8000/",
-        students:[],
-        pageStudents:[],//分页后当前页的数据
-        total:0, //数据总行数
-        currentpage:1,//当前页码
-        pagesize:10,//每页显示行数
-        inputStr: '',//输入查询条件
-        dialogTitle:'',//弹出框的标题
-        isView:false,//是否是查看
-        isEdit:false,//是否是修改
-        dialogVisible: false,
-        studentForm:{
-            sno:'',
-            name:'',
-            gender:'',
-            birthday:'',
-            mobile:'',
-            email:'',
-            address:'',
-            imaged:'',
-        },
-        rules:{
-            sno:[
-                {required:true, message:'学号不能为空', trigger:'blur'},
-                {pattern:/^[9][5]\d{3}$/, message:'学号必须是95开头的5位数字', trigger:'blur'}
-
-            ],
-            name:[
-                {required:true, message:'姓名不能为空', trigger:'blur'},
-                {pattern:/^[\u4e00-\u9fa5]{2,5}$/ ,message:'姓名在2-5汉字', trigger:'blur'}
-            ],
-            gender:[
-                {required:true, message:'性别不能为空', trigger:'change'},
-                
-            ],
-            birthday:[
-                {type:'date',required:true, message:'生日不能为空', trigger:'change'},
-            ],
-            mobile:[
-                {required:true, message:'手机号不能为空', trigger:'blur'},
-                {pattern:/^1[3-9]\d{9}$/, message:'手机号格式不正确', trigger:'blur'}
-            ],
-            email:[
-                {required:true, message:'邮箱不能为空', trigger:'blur'},
-                {pattern:/^\w+@\w+\.\w+$/, message:'邮箱格式不正确', trigger:'blur'}
-            ],
-            address:[
-                {required:true, message:'地址不能为空', trigger:'blur'},
-            ],
-        }
-    },
-    mounted() {
-        //自动加载数据
-        this.getStudents();
-    },
-    methods: {
-        //获取所有学生的信息
-        getStudents:function(){
-            //使用Axios实现Ajax请求
-            // axios.get(this.baseURL + "students/")
-            // .then(function(response){
-            //     //成功
-            //     console.log(response);
-            // })
-            // .catch(function(err){
-            //     //失败
-            //     console.log(error);
-            // });
-            //使用Axios实现Ajax请求
-            // 记录this地址
-            // let that = this; 目前我还有遇到这个问题 
-            axios.get(this.baseURL + "students/")
-            .then(response => {
-                //成功
-                // console.log(response);
-                if(response.data.code == 1) {
-                    //把数据给students
-                    this.students = response.data.data;
-                    //获取返回记录的总行数
-                    this.total = response.data.data.length;
-                    //获取当前页的数据
-                    this.getPageStudents();
-                    //提示
-                    this.$message({
-                        message: '数据加载成功',
-                        type: 'success'
-                    });
-                } else {
-                    //提示
-                    this.$message.error(response.data.msg);
-                }
-            })
-            .catch(err => {
-                //失败
-                console.log(err);
-            });
-        },
-        //添加学生时候打开表单
-        addStudent(){
-            this.dialogTitle = '添加学生明细';
-            this.dialogVisible=true;
-            
-        },
-        //查看学生的明细
-        viewStudent(row) {
-            this.dialogTitle = '查看学生明细';
-            this.isView=true;
-            this.dialogVisible=true;
-            //赋值
-            // this.studentForm = row;//这是一个浅拷贝， 会把表单中的数据一块删除掉
-            //深拷贝01就是每个元素都赋值
-            //深拷贝02
-            this.studentForm = JSON.parse(JSON.stringify(row));
-            
-        },
-         //修改学生的明细
-         updateStudent(row) {
-            this.dialogTitle = '修改学生明细';
-            //修改变量
-            this.isEdit=true;
-            this.dialogVisible=true;
-            this.studentForm = JSON.parse(JSON.stringify(row));
-            
-        },
-        //全部按钮触发的事件
-        getAllStudents(){
-            //清空
-            this.inputStr = '';
-            this.getStudents();
-        },
-        //获取当前页的学生
-        getPageStudents() {
-            //清空PageStudents中的数据
-            this.pageStudents = [];
-            //获得当前页的数据
-            for (let i=(this.currentpage-1) * this.pagesize; i<this.total; i++) {
-                //遍历数据添加到pageStudents中
-                this.pageStudents.push(this.students[i]);
-                //判断是否达到一页的要求
-                if (this.pageStudents.length >= this.pagesize) {
-                    break;
-                }
+  el: "#app",
+  data() {
+    //校验学号是否存在
+    const rulesSNo = (rule, value, callback) => {
+      //使用Axios进行校验
+      axios
+        .post(this.baseURL + "sno/check/", {
+          sno: value,
+        })
+        .then((response) => {
+          //成功
+          if (response.data.code == 1) {
+            //提示
+            if (response.data.exists) {
+              callback(new Error("学号已经存在"));
             }
-            
-        },
-        //关闭弹出框的表单
-        closeDialogForm() {
-            //清空表单
-            this.studentForm.sno='';
-            this.studentForm.name='';
-            this.studentForm.gender='';
-            this.studentForm.birthday='';
-            this.studentForm.mobile='';
-            this.studentForm.email='';
-            this.studentForm.address='';
-
-            //关闭弹出框
-            this.dialogVisible = false;
-            this.isEdit = false;
-            this.isView = false;
-        },
-        //分页时修改每页的行数
-        handleSizeChange(size){
-            //修改当前每页的行数
-            this.pagesize = size;
-            //数据重新分页
+          } else {
+            //提示
+            callback(new Error("校验学号失败,后端出现异常"));
+          }
+        })
+        .catch((err) => {
+          //失败
+          console.log(err);
+          callback(new Error("校验学号出现异常"));
+        });
+    };
+    return {
+      msg: "Hello Vue!",
+      baseURL: "http://127.0.0.1:8000/",
+      students: [],
+      pageStudents: [], //分页后当前页的数据
+      total: 0, //数据总行数
+      currentpage: 1, //当前页码
+      pagesize: 10, //每页显示行数
+      inputStr: "", //输入查询条件
+      dialogTitle: "", //弹出框的标题
+      isView: false, //是否是查看
+      isEdit: false, //是否是修改
+      dialogVisible: false,
+      studentForm: {
+        sno: "",
+        name: "",
+        gender: "",
+        birthday: "",
+        mobile: "",
+        email: "",
+        address: "",
+        imaged: "",
+      },
+      rules: {
+        sno: [
+          { required: true, message: "学号不能为空", trigger: "blur" },
+          {
+            pattern: /^[9][5]\d{3}$/,
+            message: "学号必须是95开头的5位数字",
+            trigger: "blur",
+          },
+          {
+            validator: rulesSNo,
+            trigger: "blur",
+          },
+        ],
+        name: [
+          { required: true, message: "姓名不能为空", trigger: "blur" },
+          {
+            pattern: /^[\u4e00-\u9fa5]{2,5}$/,
+            message: "姓名在2-5汉字",
+            trigger: "blur",
+          },
+        ],
+        gender: [
+          { required: true, message: "性别不能为空", trigger: "change" },
+        ],
+        birthday: [
+          {
+            type: "date",
+            required: true,
+            message: "生日不能为空",
+            trigger: "change",
+          },
+        ],
+        mobile: [
+          { required: true, message: "手机号不能为空", trigger: "blur" },
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: "手机号格式不正确",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          { required: true, message: "邮箱不能为空", trigger: "blur" },
+          {
+            pattern: /^\w+@\w+\.\w+$/,
+            message: "邮箱格式不正确",
+            trigger: "blur",
+          },
+        ],
+        address: [{ required: true, message: "地址不能为空", trigger: "blur" }],
+      },
+    };
+  },
+  mounted() {
+    //自动加载数据
+    this.getStudents();
+  },
+  methods: {
+    //获取所有学生的信息
+    getStudents: function () {
+      //使用Axios实现Ajax请求
+      // axios.get(this.baseURL + "students/")
+      // .then(function(response){
+      //     //成功
+      //     console.log(response);
+      // })
+      // .catch(function(err){
+      //     //失败
+      //     console.log(error);
+      // });
+      //使用Axios实现Ajax请求
+      // 记录this地址
+      // let that = this; 目前我还有遇到这个问题
+      axios
+        .get(this.baseURL + "students/")
+        .then((response) => {
+          //成功
+          // console.log(response);
+          if (response.data.code == 1) {
+            //把数据给students
+            this.students = response.data.data;
+            //获取返回记录的总行数
+            this.total = response.data.data.length;
+            //获取当前页的数据
             this.getPageStudents();
-
-        },
-        //实现学生信息的查询
-        queryStudents() {
-          //实用Ajax请求--post--传递查询条件
-            axios
-            .post(this.baseURL + "students/query/", {
-                inputstr: this.inputStr
-            }).then(response => {
-                //成功
-                if(response.data.code == 1) {
-                    //把数据给students
-                    this.students = response.data.data;
-                    //获取返回记录的总行数
-                    this.total = response.data.data.length;
-                    //获取当前页的数据
-                    this.getPageStudents();
-                    //提示
-                    this.$message({
-                        message: '数据查询成功',
-                        type: 'success'
-                    });
-                } else {
-                    //提示
-                    this.$message.error(response.data.msg);
-                }
-            }).catch(err => {
-                //失败
-                console.log(err);
-                this.$message.error("获取后端查询接口出现异常!");
+            //提示
+            this.$message({
+              message: "数据加载成功",
+              type: "success",
             });
-        },
-        //调整当前的页码
-        handleCurrentChange(pageNumber){
-            //修改当前的页码
-            this.currentpage = pageNumber;
-            //数据重新分页
-            this.getPageStudents();
-        },  
+          } else {
+            //提示
+            this.$message.error(response.data.msg);
+          }
+        })
+        .catch((err) => {
+          //失败
+          console.log(err);
+        });
     },
-})
+    //添加学生时候打开表单
+    addStudent() {
+      this.dialogTitle = "添加学生明细";
+      this.dialogVisible = true;
+    },
+    //查看学生的明细
+    viewStudent(row) {
+      this.dialogTitle = "查看学生明细";
+      this.isView = true;
+      this.dialogVisible = true;
+      //赋值
+      // this.studentForm = row;//这是一个浅拷贝， 会把表单中的数据一块删除掉
+      //深拷贝01就是每个元素都赋值
+      //深拷贝02
+      this.studentForm = JSON.parse(JSON.stringify(row));
+    },
+    //修改学生的明细
+    updateStudent(row) {
+      this.dialogTitle = "修改学生明细";
+      //修改变量
+      this.isEdit = true;
+      this.dialogVisible = true;
+      this.studentForm = JSON.parse(JSON.stringify(row));
+    },
+    //全部按钮触发的事件
+    getAllStudents() {
+      //清空
+      this.inputStr = "";
+      this.getStudents();
+    },
+    //获取当前页的学生
+    getPageStudents() {
+      //清空PageStudents中的数据
+      this.pageStudents = [];
+      //获得当前页的数据
+      for (
+        let i = (this.currentpage - 1) * this.pagesize;
+        i < this.total;
+        i++
+      ) {
+        //遍历数据添加到pageStudents中
+        this.pageStudents.push(this.students[i]);
+        //判断是否达到一页的要求
+        if (this.pageStudents.length >= this.pagesize) {
+          break;
+        }
+      }
+    },
+    //关闭弹出框的表单
+    closeDialogForm() {
+      //清空表单
+      this.studentForm.sno = "";
+      this.studentForm.name = "";
+      this.studentForm.gender = "";
+      this.studentForm.birthday = "";
+      this.studentForm.mobile = "";
+      this.studentForm.email = "";
+      this.studentForm.address = "";
+
+      //关闭弹出框
+      this.dialogVisible = false;
+      this.isEdit = false;
+      this.isView = false;
+    },
+    //分页时修改每页的行数
+    handleSizeChange(size) {
+      //修改当前每页的行数
+      this.pagesize = size;
+      //数据重新分页
+      this.getPageStudents();
+    },
+    //实现学生信息的查询
+    queryStudents() {
+      //实用Ajax请求--post--传递查询条件
+      axios
+        .post(this.baseURL + "students/query/", {
+          inputstr: this.inputStr,
+        })
+        .then((response) => {
+          //成功
+          if (response.data.code == 1) {
+            //把数据给students
+            this.students = response.data.data;
+            //获取返回记录的总行数
+            this.total = response.data.data.length;
+            //获取当前页的数据
+            this.getPageStudents();
+            //提示
+            this.$message({
+              message: "数据查询成功",
+              type: "success",
+            });
+          } else {
+            //提示
+            this.$message.error(response.data.msg);
+          }
+        })
+        .catch((err) => {
+          //失败
+          console.log(err);
+          this.$message.error("获取后端查询接口出现异常!");
+        });
+    },
+    //调整当前的页码
+    handleCurrentChange(pageNumber) {
+      //修改当前的页码
+      this.currentpage = pageNumber;
+      //数据重新分页
+      this.getPageStudents();
+    },
+  },
+});
